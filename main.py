682fd56c0c921b8f93a9919c61c87d7448667c3e
Diego Torres-DIEGOTOR <diegotor@motorola.com>
2022-08-26 19:37:37 -0400

    boxplot e histograma

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+filmes  = pd.read_csv("movies.csv")
 
 notas = pd.read_csv("ratings.csv")
 notas.columns = ["IdUsuario", "IdFilme", "Nota", "timestamp"]
-print(notas.head())
-#print(notas['rating'].mean())
+# print(notas.head())
+# #print(notas['rating'].mean())
+#
+# #notas.Nota.plot(kind='hist')
+# #sns.load_dataset(notas.rating)
+# sns.boxplot(x = notas["Nota"])
+# plt.show()
+#
+# print(notas['Nota'].describe())
 
-#notas.Nota.plot(kind='hist')
-#sns.load_dataset(notas.rating)
-sns.boxplot(x = notas["Nota"])
+# print(filmes.head())
+print(notas.query("IdFilme==1").Nota.mean())
+
+media_filmes = notas.groupby("IdFilme").mean()["Nota"]
+
+print(media_filmes.head())
+
+media_filmes.plot(kind='hist')
 plt.show()
-
-print(notas['Nota'].describe())
