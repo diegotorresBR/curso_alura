f16e24152aaa2d799c39d38ddcf2ade61c362c15
Diego Torres <diegopaivatorres@gmail.com>
2022-09-23 13:57:54 -0400

    Curso Alura - Data Science

--- aula3.py
+++ aula3.py
@@ -14,7 +14,7 @@ filmes = pd.read_csv("tmdb_5000_movies.csv")
 
 linguas_contagem = filmes["original_language"].value_counts().to_frame().reset_index()
 linguas_contagem.columns = ["lingua_original", "total"]#renomeando as colunas
-print(linguas_contagem.head())
+# print(linguas_contagem.head())
 
 #plotar os graficos para categorias. Usando o grafico tipo barra. Usando x e y pro grafico
 # sns.barplot(x = "lingua_original", y = "total", data = linguas_contagem)
@@ -27,17 +27,23 @@ print(linguas_contagem.head())
 # plt.pie(linguas_contagem["total"], labels=linguas_contagem["lingua_original"])
 # plt.show()
 
-print(filmes["original_language"].value_counts().loc["en"])#contar por parametro passado
+# print(filmes["original_language"].value_counts().loc["en"])#contar por parametro passado
 
 #para plotar um grafico de melhor visualização. É melhor isolar o Ingles que tem muitas ocorrências e comparas ao total das demais
-ingles = filmes["original_language"].value_counts().loc["en"]
-demais_linguas = filmes["original_language"].value_counts().sum() - ingles
+# ingles = filmes["original_language"].value_counts().loc["en"]
+# demais_linguas = filmes["original_language"].value_counts().sum() - ingles
 
 #vamos criar um dataFrame. Usando dictionary do Python
-dados = {
-    'lingua' : ['ingles', 'outros'],
-    'total' : [ingles, demais_linguas]
-}
-df_dados = pd.DataFrame(dados)
-sns.barplot(x = "lingua", y="total", data = df_dados)
+# dados = {
+#     'lingua' : ['ingles', 'outros'],
+#     'total' : [ingles, demais_linguas]
+# }
+# df_dados = pd.DataFrame(dados)
+# sns.barplot(x = "lingua", y="total", data = df_dados)
+# plt.show()
+
+
+#obter e plotar os dados sem o idioma ingles
+outras_liguas = filmes.query("original_language != 'en'")
+sns.catplot(x="original_language", kind="count", data = outras_liguas)
 plt.show()
